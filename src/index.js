06697bba4f367b8d211268d91691b79cf00f272d
Vitaliy Miles <vitaly.burbyga@gmail.com>
2022-12-07 13:39:05 +0300

    initproject

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Users from './components/users'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Users />)
+reportWebVitals()
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import Users from './components/users'
+// import './index.css'
+// import reportWebVitals from './reportWebVitals'
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <React.StrictMode>
+//     <Users />
+//   </React.StrictMode>
+// )
+// reportWebVitals()
