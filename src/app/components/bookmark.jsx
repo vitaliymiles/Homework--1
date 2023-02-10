537159a14c6aef914072b0e7819cf72c0d05600a
Vitaliy Miles <vitaly.burbyga@gmail.com>
2023-02-10 19:28:57 +0300

    перед заданием

--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 const BookMark = ({ status, ...rest }) => {
+    console.log({ ...rest })
     return (
         <button {...rest}>
             <i className={'bi bi-bookmark' + (status ? '-heart-fill' : '')}></i>
