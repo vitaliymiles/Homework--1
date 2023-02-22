c90eb0e1cb0ccd30f6e994050628050f167fb5c6
Vitaliy Miles <vitaly.burbyga@gmail.com>
2023-02-22 10:31:29 +0300

    маршрутизация

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import NavBar from './components/navBar'
+import { Route, Switch } from 'react-router-dom'
+import Main from './components/main'
+import Login from './components/login'
 import Users from './components/users'
+import UserPage from './components/userPage'
 
 function App() {
-    return <Users />
+    return (
+        <div>
+            <NavBar />
+            <Switch>
+                <Route
+                    path="/users/:userId"
+                    render={(props) => <UserPage {...props} />}
+                />
+                <Route path="/users" component={Users} />
+                <Route path="/login" component={Login} />
+                <Route path="/" exact component={Main} />
+            </Switch>
+        </div>
+    )
 }
 
 export default App
