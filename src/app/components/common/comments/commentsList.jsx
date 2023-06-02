534d277509f96c1e0cb60989aeda1e07ed2522d1
Vitaliy Miles <vitaly.burbyga@gmail.com>
2023-06-02 19:18:52 +0300

    перед заданием

--- src/app/components/common/comments/commentsList.jsx
+++ src/app/components/common/comments/commentsList.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Comment from './comment'
+import PropTypes from 'prop-types'
 
-const CommentList = ({ comments, onRemove }) => {
+const CommentsList = ({ comments, onRemove }) => {
     return comments.map((comment) => (
         <Comment key={comment._id} {...comment} onRemove={onRemove} />
     ))
 }
-CommentList.propTypes = {
-    comments: PropTypes.array,
+CommentsList.propTypes = {
+    comment: PropTypes.array,
     onRemove: PropTypes.func
 }
-export default CommentList
+
+export default CommentsList
--- src/app/components/ui/userCard.jsx
+++ src/app/components/ui/userCard.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
 import PropTypes from 'prop-types'
-
+import { useHistory } from 'react-router-dom'
 const UserCard = ({ user }) => {
     const history = useHistory()
-
     const handleClick = () => {
         history.push(history.location.pathname + '/edit')
     }
@@ -24,10 +22,8 @@ const UserCard = ({ user }) => {
                         )
                             .toString(36)
                             .substring(7)}.svg`}
-                        className="rounded-circle shadow-1-strong me-3"
-                        alt="avatar"
-                        width="250"
-                        height="250"
+                        className="rounded-circle"
+                        width="150"
                     />
                     <div className="mt-3">
                         <h4>{user.name}</h4>
@@ -54,4 +50,5 @@ const UserCard = ({ user }) => {
 UserCard.propTypes = {
     user: PropTypes.object
 }
+
 export default UserCard
